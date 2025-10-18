34cd98069b68d10777cc837e5aff2f473a59dba6
Henry Nudd <henry.nudd@gmail.com>
2025-10-17 18:59:52 -0700

    added youtube

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,10 @@ import TheWelcome from './components/TheWelcome.vue'
       <HelloWorld msg="You did it!" />
     </div>
   </header>
-
+  <div>
+  </div>
   <main>
+   
     <TheWelcome />
   </main>
 </template>
@@ -26,21 +28,4 @@ header {
   margin: 0 auto 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -7,7 +7,7 @@
 
     <h1 style="font-family: 'Segoe Print';display:grid; justify-content: center;">Goose Desktop Pet</h1>
   <div class="goosy">
-      <img src="Goosy.PNG">
+      <img src="../assets/Goosy.PNG">
       <a href="https://samperson.itch.io/desktop-goose" style = "padding-left: 30px;">
       <button class= "Download_Button">Download</button>
   </a>
@@ -17,3 +17,12 @@
 
   
 </template>
+
+<style scoped>
+
+    .goosy{
+        display: grid;
+        justify-content: center;
+        padding-top: 100px;
+    }
+</style>
